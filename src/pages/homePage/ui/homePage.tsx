4d6bac7032593575e6866fecc040dc4d3f23c9d7
AushaL <thetingler85@gmail.com>
2024-08-10 15:22:51 +0300

    feat: add home page sections

--- src/pages/homePage/ui/homePage.tsx
+++ src/pages/homePage/ui/homePage.tsx
@@ -1,13 +1,19 @@
 import { FC } from "react";
 import { Helmet } from "react-helmet-async";
 
+import { Hero } from "../../../widgets/hero";
+import { Faq } from "../../../widgets/faq";
+
 export const HomePage: FC = () => {
   return (
     <>
       <Helmet>
         <title>Application Main page</title>
       </Helmet>
-      <div>Главная</div>
+      <div>
+        <Hero />
+        <Faq />
+      </div>
     </>
   );
 };
---
+++ src/widgets/hero/ui/hero.tsx
@@ -0,0 +1,53 @@
+import { FC } from "react";
+import { Container, Text, Button, Group } from "@mantine/core";
+import { GithubIcon } from "@mantinex/dev-icons";
+import "./hero.scss";
+
+export const Hero: FC = () => {
+  return (
+    <div className="hero__wrapper">
+      <Container size={700} className="hero__inner">
+        <h1 className="hero__title">
+          A{" "}
+          <Text
+            component="span"
+            variant="gradient"
+            gradient={{ from: "blue", to: "cyan" }}
+            inherit
+          >
+            fully featured
+          </Text>{" "}
+          React components and hooks library
+        </h1>
+
+        <Text className="hero__description" color="dimmed">
+          Build fully functional accessible web applications with ease – Mantine
+          includes more than 100 customizable components and hooks to cover you
+          in any situation
+        </Text>
+
+        <Group className="hero__controls">
+          <Button
+            size="xl"
+            className="hero__control"
+            variant="gradient"
+            gradient={{ from: "blue", to: "cyan" }}
+          >
+            Get started
+          </Button>
+
+          <Button
+            component="a"
+            href="https://github.com/mantinedev/mantine"
+            size="xl"
+            variant="default"
+            className="hero__control"
+            leftSection={<GithubIcon size={20} />}
+          >
+            GitHub
+          </Button>
+        </Group>
+      </Container>
+    </div>
+  );
+};
